176eb1b7330f0cd318868827d0d4065e364dc458
Weslei Bruno <weslei945@gmail.com>
2020-09-24 15:11:03 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+const relogio = document.querySelector('.relogio');
+const iniciar = document.querySelector('.iniciar');
+const pausar = document.querySelector('.pausar');
+const zerar = document.querySelector('.zerar');
