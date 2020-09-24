9f67d2b0147a1b04a9ade90e7002bfc04e7a7954
Weslei Bruno <weslei945@gmail.com>
2020-09-24 16:30:32 -0300

    seventh

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,61 @@
-function criaHoraDosSegundos(segundos) {
-    const data = new Date(segundos * 1000);
-    return data.toLocaleTimeString('pt-BR', {
-        hour12: false,
-        timeZone: 'GMT'
+function relogio() {
+    function criaHoraDosSegundos(segundos) {
+        const data = new Date(segundos * 1000);
+        return data.toLocaleTimeString('pt-BR', {
+            hour12: false,
+            timeZone: 'GMT'
+        });
+    }
+    
+    console.log(criaHoraDosSegundos(10))
+    
+    const relogio = document.querySelector('.relogio');
+    const iniciar = document.querySelector('.iniciar');
+    const pausar = document.querySelector('.pausar');
+    const zerar = document.querySelector('.zerar');
+    let segundos = 0;
+    let timer;
+    
+    function iniciaRelogio() {
+        timer = setInterval(function() {
+            segundos++;
+            relogio.innerHTML = criaHoraDosSegundos(segundos);
+        }, 1000)
+    }
+    
+    document.addEventListener('click', function(e) {
+        const elemento = e.target;
+        // console.log(elemento);
+        if(elemento.classList.contains('iniciar')) {
+            relogio.classList.remove('pausado');
+            clearInterval(timer);
+            iniciaRelogio();
+        }
+        if(elemento.classList.contains('pausar')) {
+            clearInterval(timer);
+            relogio.classList.add('pausado');
+        }
+        if(elemento.classList.contains('zerar')) {
+            clearInterval(timer);
+            relogio.innerHTML = '00:00:00';
+            relogio.classList.remove('pausado');
+            segundos = 0;
+        }
     });
+    
+    // iniciar.addEventListener('click', function(event) {
+    //     relogio.classList.remove('pausado');
+    //     clearInterval(timer);
+    //     iniciaRelogio();
+    // });
+    // pausar.addEventListener('click', function(event) {
+    //     clearInterval(timer);
+    //     relogio.classList.add('pausado');
+    // });
+    // zerar.addEventListener('click', function(event) {
+    //     clearInterval(timer);
+    //     relogio.innerHTML = '00:00:00';
+    //     segundos = 0;
+    // });
 }
-
-console.log(criaHoraDosSegundos(10))
-
-const relogio = document.querySelector('.relogio');
-const iniciar = document.querySelector('.iniciar');
-const pausar = document.querySelector('.pausar');
-const zerar = document.querySelector('.zerar');
-let segundos = 0;
-
-function iniciaRelogio() {
-    const timer = setInterval(function() {
-        segundos++;
-        relogio.innerHTML = criaHoraDosSegundos(segundos);
-    }, 1000)
-}
-
-iniciar.addEventListener('click', function(event) {
-    iniciaRelogio();
-});
-pausar.addEventListener('click', function(event) {
-    relogio.innerHTML = 'Cliquei no pausar';
-});
-zerar.addEventListener('click', function(event) {
-    relogio.innerHTML = 'Cliquei no zerar';
-});
+relogio();
